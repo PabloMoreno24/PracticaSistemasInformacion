9465b8a922e6f5519979f943f4a3a59532784ce7
Rubén <rubenrosacarrasco@gmail.com>
2022-03-15 17:38:20 +0100

    prueba users

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ data = json.load(f)
 con = sqlite3.connect('practica.db')
 cursor_obj = con.cursor()
 cursor_obj.execute("DROP TABLE legal")
+cursor_obj.execute("DROP TABLE users")
 cursor_obj.execute("CREATE TABLE IF NOT EXISTS legal (nombre,cookies,aviso,proteccion_de_datos,creacion, primary key(nombre))")
 cursor_obj.execute("CREATE TABLE IF NOT EXISTS users (id,nombre,telefono,password,provincia,permisos,total_emails,phishing_email,ciclados_email,fechas,num_fechas,ips,num_ips,primary key (id))")
 inser = """INSERT INTO legal (nombre,cookies,aviso,proteccion_de_datos,creacion) VALUES (?,?,?,?,?)"""
@@ -19,6 +20,13 @@ for i in data['legal']:
         for k in i.values():
             datos = (j, k['cookies'], k['aviso'], k['proteccion_de_datos'], k['creacion'])
 
+        cursor_obj.execute(inser, datos)
+        con.commit()
+
+for i in data['users']:
+    for j in i.keys():
+        for k in i.values():
+            datos = (j, k['telefono'], k['contrasena'], k['provincia'], k['permisos'], k['emails']['total'], k['emails']['phising'], k['emails']['cliclados'], k['fechas'], len(k['fechas']), k['ips'], len(k['ips']))
         cursor_obj.execute(inser, datos)
         con.commit()
 
