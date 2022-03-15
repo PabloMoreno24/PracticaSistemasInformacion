c20e6d40406fa81bfcff31f518e51df78e660003
Victor <victorhf2001@gmail.com>
2022-03-15 17:11:25 +0100

    Prueba Insert

--- app.py
+++ app.py
@@ -12,6 +12,14 @@ con = sqlite3.connect('practica.db')
 cursor_obj = con.cursor()
 cursor_obj.execute("CREATE TABLE IF NOT EXISTS legal (nombre,cookies,aviso,proteccion_de_datos,creacion, primary key(nombre))")
 cursor_obj.execute("CREATE TABLE IF NOT EXISTS users (id,nombre,telefono,password,provincia,permisos,total_emails,phishing_email,ciclados_email,fechas,num_fechas,ips,num_ips,primary key (id))")
+inser = """INSERT INTO legal (nombre) VALUES (?)"""
+
+for i in data['legal']:
+    for j in i.keys():
+        datos = (j)
+        cursor_obj.execute(inser, datos)
+        con.commit()
+
 con.commit()
 con.close()
 
