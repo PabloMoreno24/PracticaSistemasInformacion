d16c644fea9367d0e34f02a1d19d511e1a26fe0a
pablo <pablomorenoaranda@gmail.com>
2022-03-15 16:59:29 +0100

    Initial Commit

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from flask import Flask
+import sqlite3
+import json
 
 app = Flask(__name__)
 
+f = open('legal.json')
+data = json.load(f)
+
+
+con = sqlite3.connect('practica.db')
+cursor_obj = con.cursor()
+cursor_obj.execute("CREATE TABLE IF NOT EXISTS legal (nombre,cookies,aviso,proteccion_de_datos,creacion, primary key(nombre))")
+cursor_obj.execute("CREATE TABLE IF NOT EXISTS users (id,nombre,telefono,password,provincia,permisos,total_emails,phishing_email,ciclados_email,fechas,num_fechas,ips,num_ips,primary key (id))")
+con.commit()
+con.close()
 
 @app.route('/')
 def hello_world():  # put application's code here
